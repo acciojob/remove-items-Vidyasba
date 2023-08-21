ea59db3f027a59c2cd6b385913a8ba46522003ee
Vidyasba <116746304+Vidyasba@users.noreply.github.com>
2023-08-21 11:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let getbtn= document.getElementById("removeButton");
+window.addEventlistner("click", removeitem());
+
+function removeitem(){
+	let getitem= document.getElementById("colorSelect");
+	const selectedindex=getitem.selectedindex;
+	if(selectedindex!== -1){
+		getitem.remove(selectedindex);
+	}
+}
